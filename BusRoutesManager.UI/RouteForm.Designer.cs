4231b66bb8d7d84b4e8f624616b585279cf3179d
Maxym Tomusiak <maxtomusiak315@gmail.com>
2024-04-02 05:06:37 +0300

    feat: finish user form

---
+++ BusRoutesManager.UI/RouteForm.Designer.cs
@@ -0,0 +1,287 @@
+﻿namespace BusRoutesManager.UI
+{
+    partial class RouteForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            labelBusModel = new Label();
+            label2 = new Label();
+            labelBusPlate = new Label();
+            label3 = new Label();
+            labelSeatsLeft = new Label();
+            label4 = new Label();
+            labelDriverName = new Label();
+            label5 = new Label();
+            labelDriverPhone = new Label();
+            label6 = new Label();
+            labelTicketDate = new Label();
+            label7 = new Label();
+            labelFrom = new Label();
+            label8 = new Label();
+            labelTo = new Label();
+            button1 = new Button();
+            label9 = new Label();
+            labelPrice = new Label();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            label1.Location = new Point(12, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(99, 23);
+            label1.TabIndex = 0;
+            label1.Text = "Bus model:";
+            // 
+            // labelBusModel
+            // 
+            labelBusModel.AutoSize = true;
+            labelBusModel.Font = new Font("Segoe UI", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            labelBusModel.Location = new Point(117, 9);
+            labelBusModel.Name = "labelBusModel";
+            labelBusModel.Size = new Size(0, 23);
+            labelBusModel.TabIndex = 0;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            label2.Location = new Point(12, 43);
+            label2.Name = "label2";
+            label2.Size = new Size(89, 23);
+            label2.TabIndex = 0;
+            label2.Text = "Bus plate:";
+            // 
+            // labelBusPlate
+            // 
+            labelBusPlate.AutoSize = true;
+            labelBusPlate.Font = new Font("Segoe UI", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            labelBusPlate.Location = new Point(105, 43);
+            labelBusPlate.Name = "labelBusPlate";
+            labelBusPlate.Size = new Size(0, 23);
+            labelBusPlate.TabIndex = 0;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            label3.Location = new Point(12, 75);
+            label3.Name = "label3";
+            label3.Size = new Size(90, 23);
+            label3.TabIndex = 0;
+            label3.Text = "Seats left:";
+            // 
+            // labelSeatsLeft
+            // 
+            labelSeatsLeft.AutoSize = true;
+            labelSeatsLeft.Font = new Font("Segoe UI", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            labelSeatsLeft.Location = new Point(110, 75);
+            labelSeatsLeft.Name = "labelSeatsLeft";
+            labelSeatsLeft.Size = new Size(0, 23);
+            labelSeatsLeft.TabIndex = 0;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            label4.Location = new Point(363, 9);
+            label4.Name = "label4";
+            label4.Size = new Size(114, 23);
+            label4.TabIndex = 0;
+            label4.Text = "Driver name:";
+            // 
+            // labelDriverName
+            // 
+            labelDriverName.AutoSize = true;
+            labelDriverName.Font = new Font("Segoe UI", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            labelDriverName.Location = new Point(483, 9);
+            labelDriverName.Name = "labelDriverName";
+            labelDriverName.Size = new Size(0, 23);
+            labelDriverName.TabIndex = 0;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            label5.Location = new Point(363, 43);
+            label5.Name = "label5";
+            label5.Size = new Size(120, 23);
+            label5.TabIndex = 0;
+            label5.Text = "Driver phone:";
+            // 
+            // labelDriverPhone
+            // 
+            labelDriverPhone.AutoSize = true;
+            labelDriverPhone.Font = new Font("Segoe UI", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            labelDriverPhone.Location = new Point(489, 43);
+            labelDriverPhone.Name = "labelDriverPhone";
+            labelDriverPhone.Size = new Size(0, 23);
+            labelDriverPhone.TabIndex = 0;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Segoe UI", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            label6.Location = new Point(363, 75);
+            label6.Name = "label6";
+            label6.Size = new Size(105, 23);
+            label6.TabIndex = 0;
+            label6.Text = "Ticket date:";
+            // 
+            // labelTicketDate
+            // 
+            labelTicketDate.AutoSize = true;
+            labelTicketDate.Font = new Font("Segoe UI", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            labelTicketDate.Location = new Point(474, 75);
+            labelTicketDate.Name = "labelTicketDate";
+            labelTicketDate.Size = new Size(0, 23);
+            labelTicketDate.TabIndex = 0;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("Segoe UI", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            label7.Location = new Point(12, 140);
+            label7.Name = "label7";
+            label7.Size = new Size(57, 23);
+            label7.TabIndex = 0;
+            label7.Text = "From:";
+            // 
+            // labelFrom
+            // 
+            labelFrom.AutoSize = true;
+            labelFrom.Font = new Font("Segoe UI", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            labelFrom.Location = new Point(79, 140);
+            labelFrom.Name = "labelFrom";
+            labelFrom.Size = new Size(0, 23);
+            labelFrom.TabIndex = 0;
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Font = new Font("Segoe UI", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            label8.Location = new Point(12, 173);
+            label8.Name = "label8";
+            label8.Size = new Size(33, 23);
+            label8.TabIndex = 0;
+            label8.Text = "To:";
+            // 
+            // labelTo
+            // 
+            labelTo.AutoSize = true;
+            labelTo.Font = new Font("Segoe UI", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            labelTo.Location = new Point(51, 173);
+            labelTo.Name = "labelTo";
+            labelTo.Size = new Size(0, 23);
+            labelTo.TabIndex = 0;
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            button1.Location = new Point(361, 119);
+            button1.Name = "button1";
+            button1.Size = new Size(285, 77);
+            button1.TabIndex = 1;
+            button1.Text = "Buy ticket";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Font = new Font("Segoe UI", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            label9.Location = new Point(11, 112);
+            label9.Name = "label9";
+            label9.Size = new Size(99, 23);
+            label9.TabIndex = 0;
+            label9.Text = "Total price:";
+            // 
+            // labelPrice
+            // 
+            labelPrice.AutoSize = true;
+            labelPrice.Font = new Font("Segoe UI", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            labelPrice.Location = new Point(117, 112);
+            labelPrice.Name = "labelPrice";
+            labelPrice.Size = new Size(0, 23);
+            labelPrice.TabIndex = 0;
+            // 
+            // RouteForm
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(658, 224);
+            Controls.Add(button1);
+            Controls.Add(labelTo);
+            Controls.Add(labelPrice);
+            Controls.Add(labelFrom);
+            Controls.Add(labelSeatsLeft);
+            Controls.Add(labelBusPlate);
+            Controls.Add(labelTicketDate);
+            Controls.Add(labelDriverPhone);
+            Controls.Add(labelDriverName);
+            Controls.Add(labelBusModel);
+            Controls.Add(label8);
+            Controls.Add(label9);
+            Controls.Add(label7);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label1);
+            Name = "RouteForm";
+            FormClosing += RouteForm_FormClosing;
+            Load += RouteForm_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label labelBusModel;
+        private Label label2;
+        private Label labelBusPlate;
+        private Label label3;
+        private Label labelSeatsLeft;
+        private Label label4;
+        private Label labelDriverName;
+        private Label label5;
+        private Label labelDriverPhone;
+        private Label label6;
+        private Label labelTicketDate;
+        private Label label7;
+        private Label labelFrom;
+        private Label label8;
+        private Label labelTo;
+        private Button button1;
+        private Label label9;
+        private Label labelPrice;
+    }
+}
--- BusRoutesManager.UI/UserForm.cs
+++ BusRoutesManager.UI/UserForm.cs
@@ -1,4 +1,7 @@
 ﻿using BusRoutesManager.Domain.Context;
+using BusRoutesManager.Domain.Entities;
+using BusRoutesManager.Repositories;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -16,18 +19,123 @@ namespace BusRoutesManager.UI
         private readonly BusManagerContext _ctx;
         private readonly Form1 baseform;
         private readonly int userId;
+        private readonly Repository<Route> _routsRepository;
+        private readonly Repository<BusStation> _stationsRepository;
+        private readonly Repository<City> _cityRepository;
         public UserForm(Form1 form, BusManagerContext ctx, int userId)
         {
             InitializeComponent();
             baseform = form;
             _ctx = ctx;
             this.userId = userId;
+            _routsRepository = new Repository<Route>(_ctx);
+            _stationsRepository = new Repository<BusStation>(_ctx);
+            _cityRepository = new Repository<City>(_ctx);
+            
+            UpdateCities();
+            UpdateStations();
+        }
+        private void UpdateCities()
+        {
+            var cities = _cityRepository.GetAll().Select(c => $"{c.Id}){c.Title}");
+            comboBoxFromCity.Items.Clear();
+            comboBoxFromCity.Items.AddRange(cities.ToArray());
+
+            comboBoxToCity.Items.Clear();
+            comboBoxToCity.Items.AddRange(cities.ToArray());
+        }
+        private void UpdateStations()
+        {
+            if (comboBoxToCity.SelectedIndex != -1)
+            {
+                comboBoxToStation.Items.Clear();
+                City toCity = _cityRepository.GetById(int.Parse(comboBoxToCity.SelectedItem.ToString().Split(')')[0]));
+                comboBoxToStation.Items.AddRange(toCity.BusStations.Select(s => $"{s.Id}){s.Title}").ToArray());
+            }
+            if (comboBoxFromCity.SelectedIndex != -1)
+            {
+                comboBoxFromStation.Items.Clear();
+                City fromCity = _cityRepository.GetById(int.Parse(comboBoxFromCity.SelectedItem.ToString().Split(')')[0]));
+                comboBoxFromStation.Items.AddRange(fromCity.BusStations.Select(s => $"{s.Id}){s.Title}").ToArray());
+            }
         }
 
         private void UserForm_FormClosing(object sender, FormClosingEventArgs e)
         {
             baseform.Enabled = true;
             baseform.Show();
+        }
+        private void comboBoxFromCity_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            UpdateStations();
+        }
+
+        private void comboBoxToCity_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            UpdateStations();
+        }
+        private void btnFindRoutes_Click(object sender, EventArgs e)
+        {
+            if (comboBoxFromCity.SelectedIndex == -1 ||
+                comboBoxToCity.SelectedIndex == -1 ||
+                comboBoxFromStation.SelectedIndex == -1 ||
+                comboBoxToStation.SelectedIndex == -1)
+            {
+                MessageBox.Show("Select cities and stations first!");
+                return;
+            }
+            var dayOfWeek = (int)dateTimePicker.Value.DayOfWeek;
+            BusStation fromStation = _stationsRepository.GetById(int.Parse(comboBoxFromStation.SelectedItem.ToString().Split(')')[0]));
+            BusStation toStation = _stationsRepository.GetById(int.Parse(comboBoxToStation.SelectedItem.ToString().Split(')')[0]));
+            var routes = _routsRepository.GetAll()
+                .Where(r => r.DayOfWeek == dayOfWeek)
+                .Where(r => r.RouteNodes.FirstOrDefault(n => n.Station == fromStation) != null &&
+                    r.RouteNodes.FirstOrDefault(n => n.Station == toStation) != null)
+                .Where(r => r.RouteNodes.First(n => n.Station == fromStation).ArrivalTime.TimeOfDay <
+                    r.RouteNodes.First(n => n.Station == toStation).ArrivalTime.TimeOfDay)
+                .OrderBy(r => r.ArrivalTime.TimeOfDay).ToList();
+
+            // Clear the panel if it already contains any controls
+            panelRoutes.Controls.Clear();
+
+            // Define the starting position for displaying route information
+            int x = 10;
+            int y = 10;
+
+            // Iterate over the list of routes
+            Font baseFont = new Font(FontFamily.GenericSansSerif, 12);
+            foreach (Route route in routes)
+            {
+                // Create a panel for the route
+                Panel routePanel = new Panel();
+                routePanel.Tag = route.Id;
+                routePanel.BorderStyle = BorderStyle.FixedSingle;
+                routePanel.Size = new Size(this.Width - 50, 100); // Adjust size as needed
+                routePanel.Location = new Point(x, y);
+                routePanel.Tag = route.Id;
+                routePanel.DoubleClick += PanelRoute_DoubleClick;
+
+                // Create a label to display route information
+                Label routeLabel = new Label();
+                routeLabel.Text = $"Bus plate: {route.Bus.PlateNumber}, Driver: {route.Driver.FullName.ToString()}\n\nDeparture: {route.DepartureTime.ToShortTimeString()}, Arrival: {route.ArrivalTime.ToShortTimeString()}";
+                routeLabel.AutoSize = true;
+                routeLabel.Location = new Point(10, 10);
+                routePanel.Controls.Add(routeLabel);
+
+                // Add the route panel to the main panel
+                panelRoutes.Controls.Add(routePanel);
+                
+                // Increment y coordinate for the next route
+                y += 120; // Adjust this value as needed
+            }
+        }
+        private void PanelRoute_DoubleClick(object sender, EventArgs e)
+        {
+            BusStation fromStation = _stationsRepository.GetById(int.Parse(comboBoxFromStation.SelectedItem.ToString().Split(')')[0]));
+            BusStation toStation = _stationsRepository.GetById(int.Parse(comboBoxToStation.SelectedItem.ToString().Split(')')[0]));
+            this.Enabled = false;
+            RouteForm child = new RouteForm(this, _routsRepository.GetById(int.Parse((sender as Panel).Tag.ToString())), fromStation, toStation, _ctx, userId, dateTimePicker.Value);
+            child.Show();
         }
     }
 }
